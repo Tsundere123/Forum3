3b40917148be925bac43ddd09b9fb3b0b99c73cb
strangers123 <91206077+Tsundere123@users.noreply.github.com>
2023-11-24 13:25:34 +0100

    init posts files

---
+++ Forum3/ClientApp/src/app/forumPosts/forumPosts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-forum-posts',
+  templateUrl: './forum-posts.component.html',
+  styleUrls: ['./forum-posts.component.css']
+})
+export class ForumPostsComponent {
+
+}
