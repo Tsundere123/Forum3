62011e89dbb3c10d3e90947f2e6e98c39b834aca
Cenosphere <git@cenosphe.re>
2023-11-28 13:09:28 +0100

    show category contents if no error and not loading

--- Forum3/ClientApp/src/app/forumThreads/forumThreads.component.ts
+++ Forum3/ClientApp/src/app/forumThreads/forumThreads.component.ts
@@ -1,68 +1,60 @@
-import {Component, OnInit} from "@angular/core";
-import {ForumCategoriesService} from "../services/forumCategories.service";
-import {ForumCategory} from "../models/forumCategory.model";
-import {ForumThreadsService} from "../services/forumThreads.service";
-import {ForumThreadViewModel} from "../models/forumThreadView.model";
-import {ActivatedRoute} from "@angular/router";
-import {ForumCategoriesComponent} from "../forumCategories/forumCategories.component";
-import {inject} from "@angular/core/testing";
-import {ForumThread} from "../models/forumThread/forumThread.model";
-import {ForumPost} from "../models/forumPost.model";
-import {ForumPostsService} from "../services/forumPosts.service";
-import {ForumCategoryDetailsModel} from "../models/forumCategoryDetails.model";
-import {AuthorizeService} from "../../api-authorization/authorize.service";
-import {Observable} from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { ForumThreadsService } from "../services/forumThreads.service";
+import { ActivatedRoute } from "@angular/router";
+import { ForumThread } from "../models/forumThread/forumThread.model";
+import { ForumCategoryDetailsModel } from "../models/forumCategoryDetails.model";
+import { AuthorizeService } from "../../api-authorization/authorize.service";
+import { Observable } from "rxjs";
 
 @Component({
   selector: 'app-forumThreads-component',
-  templateUrl: './forumThreads.component.html',
-  styleUrls: ['./forumThreads.component.css']
+  templateUrl: './forumThreads.component.html'
 })
 
 export class ForumThreadsComponent implements OnInit{
-  public isAuthenticated?: Observable<boolean>;
-  threadsInCategory: ForumThread[] = [];
+  isLoading: boolean = true;
+  isError: boolean = false;
+
+  isAuthenticated?: Observable<boolean>;
+
   pinnedThreadsInCategory: ForumThread[] = [];
   unpinnedThreadsInCategory: ForumThread[] = [];
+
+  categoryId: number = 0;
   categoryDetails: ForumCategoryDetailsModel;
-  categoryId:number = 0;
-  constructor(private forumThreadsServices: ForumThreadsService, private route:ActivatedRoute, private authorizeService: AuthorizeService) { }
+
+  constructor(
+    private authorizeService: AuthorizeService,
+    private forumThreadsServices: ForumThreadsService,
+    private route: ActivatedRoute
+  ) { }
+
   ngOnInit(): void {
     this.isAuthenticated = this.authorizeService.isAuthenticated();
     this.route.params.subscribe(params => this.categoryId = +params['id']);
 
     this.forumThreadsServices.ForumThreadsOfCategory(this.categoryId).subscribe({
       next:(threads) => {
-        let pinnedThreads: ForumThread[] = [];
-        let unpinnedThreads: ForumThread[] = [];
-        threads.forEach(function(thread){
-          console.log("Testing thread...")
-          console.log(thread)
-          if(thread.isPinned){
-            pinnedThreads.push(thread);
-          }else{
-            unpinnedThreads.push(thread);
-          }
-        });
-        this.pinnedThreadsInCategory = pinnedThreads;
-        this.unpinnedThreadsInCategory = unpinnedThreads;
-        console.log("All threads")
-        console.log(threads);
-        console.log("Pinned threads")
-        console.log(pinnedThreads);
-        console.log("Unpinned threads")
-        console.log(unpinnedThreads);
+        this.pinnedThreadsInCategory = threads.filter(thread => thread.isPinned);
+        this.unpinnedThreadsInCategory = threads.filter(thread => !thread.isPinned);
+
+        this.isLoading = false;
       },
       error:(response) =>{
+        this.isError = true;
+        this.isLoading = false;
         console.log(response);
       }
     });
 
     this.forumThreadsServices.GetCategoryDetails(this.categoryId).subscribe({
       next:(category) =>{
-        console.log(category);
         this.categoryDetails = category;
+      },
+      error:(response) =>{
+        this.isError = true;
+        console.log(response);
       }
-    })
+    });
   }
 }
