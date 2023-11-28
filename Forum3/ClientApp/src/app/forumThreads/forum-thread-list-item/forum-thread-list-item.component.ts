ef88bba8fafee5f30a3873a505598d6b629328f8
Cenosphere <git@cenosphe.re>
2023-11-28 13:08:09 +0100

    thread list item cleanup

--- Forum3/ClientApp/src/app/forumThreads/forum-thread-list-item/forum-thread-list-item.component.ts
+++ Forum3/ClientApp/src/app/forumThreads/forum-thread-list-item/forum-thread-list-item.component.ts
@@ -1,41 +1,11 @@
-import {Component, Input} from '@angular/core';
-import {ForumThreadViewModel} from "../../models/forumThreadView.model";
-import {ForumThread} from "../../models/forumThread/forumThread.model";
-import {ForumPost} from "../../models/forumPost.model";
-import {ForumPostsService} from "../../services/forumPosts.service";
+import { Component, Input } from '@angular/core';
+import { ForumThread } from "../../models/forumThread/forumThread.model";
 
 @Component({
   selector: 'app-forum-thread-list-item',
-  templateUrl: './forum-thread-list-item.component.html',
-  styleUrls: ['./forum-thread-list-item.component.css']
+  templateUrl: './forum-thread-list-item.component.html'
 })
 
 export class ForumThreadListItemComponent {
-  @Input() viewModel: ForumThreadViewModel;
   @Input() currentThread: ForumThread;
-  latestPost: ForumPost;
-  numberOfPosts: number;
-
-  constructor(private forumPostsServices: ForumPostsService) {  }
-
-  ngOnInit(): void {
-    this.forumPostsServices.GetAllPostsOfThread(this.currentThread.id).subscribe({
-      next:(posts) => {
-        console.log(posts);
-        let latestPost = null;
-        posts.forEach(function(post){
-          latestPost = post;
-          if(post.createdAt > latestPost.createdAt){
-            latestPost = post;
-          }
-        });
-        this.latestPost = latestPost;
-        this.numberOfPosts = posts.length;
-        if(!posts.length) this.numberOfPosts = 0;
-      },
-      error:(response) =>{
-        console.log(response);
-      }
-    });
-  }
 }
