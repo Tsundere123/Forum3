cc080cef085f365404923fd9c28601846f796bf2
Cenosphere <git@cenosphe.re>
2023-11-28 13:08:49 +0100

    thread list normal icons cleanup

--- Forum3/ClientApp/src/app/forumThreads/forum-thread-list-item/forum-thread-list-normal-icons/forum-thread-list-normal-icons.component.ts
+++ Forum3/ClientApp/src/app/forumThreads/forum-thread-list-item/forum-thread-list-normal-icons/forum-thread-list-normal-icons.component.ts
@@ -1,12 +1,11 @@
 import {Component, Input} from '@angular/core';
-import { CommonModule } from '@angular/common';
 import {ForumThread} from "../../../models/forumThread/forumThread.model";
 
 @Component({
   selector: 'app-forum-thread-list-normal-icons',
-  templateUrl: './forum-thread-list-normal-icons.component.html',
-  styleUrl: './forum-thread-list-normal-icons.component.css'
+  templateUrl: './forum-thread-list-normal-icons.component.html'
 })
+
 export class ForumThreadListNormalIconsComponent {
   @Input() currentThread: ForumThread;
 }
